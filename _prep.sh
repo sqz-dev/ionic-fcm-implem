a191d600a38367d69aad79cc38311bbfc4fe3557
Jhun Bryan Belen <132556761+sqz-dev@users.noreply.github.com>
2023-05-09 21:33:55 +0800

    Create cicd-sit.yml (#1)
    * Create cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update _prep.sh
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update cicd-sit.yml
    
    * Update _prep.sh
    
    * Update cicd-sit.yml

--- _prep.sh
+++ _prep.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# file_path="./package.json";
-# sed -i 's/"android"//gi' ${file_path};
-# sed -i 's/"cordova-plugin-agilaplugin": {},//gi' ${file_path};
-# # sed -i 's/"cordova-plugin-agilaplugin": {}//gi' ${file_path};
-# # sed -i 's/"cordova-plugin-local-notification": {},/"cordova-plugin-local-notification": {}/gi' ${file_path};
-
-rm -rf ./.sourcemaps ./node_modules ./platforms ./plugins ./www ./package-lock.json;
-npm cache clean --force;
-npm i --prefer-online;
-ionic cordova platform add android@8 --no-resources;
-ionic cordova plugin add ./AgilaPlugin;
-cp ./google-services.json ./platforms/android/app;
-ionic cordova build android --configuration=buildDccUat;
+npm i;
+ionic cordova platform add android@8;
+ionic cordova build android;
+ionic cordova build android --release --prod;
