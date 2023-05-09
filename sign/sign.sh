1b850c875e4311d4b8fdeee2c0ca674dbeb58ca6
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-09 21:49:23 +0800

    CICD to sign built release apk

--- sign/sign.sh
+++ sign/sign.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# echo "Enter Build Environment:"
-# read env
-
-signer_dir=$(pwd)
-
-# change this to where your source code is. Note: if your code is in drive C, it should start with /c/ and not C:\
-src_dir="/j/dev/ionic-fcm-implem"
-build_dir="${src_dir}/platforms/android/app/build/outputs/apk/release"
-
-cp ${build_dir}/app-release-unsigned.apk ${signer_dir}
-
 env="APP"
-ver=$(grep -o 'version="[0-9.]*"' ${src_dir}/config.xml | grep -o '[0-9.]*')
-verCon=$(grep -o 'android-versionCode="[^"]*"' ${src_dir}/config.xml | grep -o '[0-9]*')
+ver=$(grep -o 'version="[0-9.]*"' config.xml | grep -o '[0-9.]*')
+verCon=$(grep -o 'android-versionCode="[^"]*"' config.xml | grep -o '[0-9]*')
 date=$(date +%m%d%Y)
-pass="Nooneknowsthis1"
+pass=${{ secrets.GITHUB_KEYPASS }}
 apk_name="AZ-Journey_${env^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ionicdemo.jks -storepass "$pass" -keypass "$pass" app-release-unsigned.apk ionicdemo
-zipalign -v -p -f 4 app-release-unsigned.apk ${apk_name}
-
-start ${signer_dir}
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sign/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/app-release-unsigned.apk ionicdemo
+zipalign -v -p -f 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk platforms/android/app/build/outputs/apk/${apk_name}
