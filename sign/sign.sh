2d7903a0ab8e80a5bffbf04ad4c5eb8d7eb3bef1
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-09 22:56:59 +0800

    Test passing github workspace to custom script

--- sign/sign.sh
+++ sign/sign.sh
@@ -7,7 +7,8 @@ date=$(date +%m%d%Y)
 pass=${1}
 apkName="AZ-Journey_${env^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
 
-signerDir="/home/runner/work/ionic-fcm-implem"
+signerDir="home/runner/work/ionic-fcm-implem"
+var2=${2}
 
 echo "signerDir: ${signerDir}";
 echo "Ver: ${ver}";
@@ -15,6 +16,7 @@ echo "verCon: ${verCon}";
 echo "date: ${date}";
 echo "pass: ${pass}";
 echo "apkName: ${apkName}";
+echo "var2: ${var2}";
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sign/ionicdemo.jks -storepass "$pass" -keypass "$pass" ${signerDir}/platforms/android/app/build/outputs/apk/app-release-unsigned.apk ionicdemo
 /usr/local/lib/android/sdk/build-tools/27.0.3/zipalign -v -p -f 4 ${signerDir}/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ${signerDir}/platforms/android/app/build/outputs/apk/${apkName}
