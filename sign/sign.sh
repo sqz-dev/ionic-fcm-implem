da57b3f9ba07d6fa791e271c7fe7e8a004c280c4
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-09 21:58:29 +0800

    Passed secret in custom script

--- sign/sign.sh
+++ sign/sign.sh
@@ -4,7 +4,7 @@ env="APP"
 ver=$(grep -o 'version="[0-9.]*"' config.xml | grep -o '[0-9.]*')
 verCon=$(grep -o 'android-versionCode="[^"]*"' config.xml | grep -o '[0-9]*')
 date=$(date +%m%d%Y)
-pass=${{ secrets.GITHUB_KEYPASS }}
+pass=${1}
 apk_name="AZ-Journey_${env^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sign/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/app-release-unsigned.apk ionicdemo
