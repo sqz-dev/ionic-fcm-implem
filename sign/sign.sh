2d9f5a17be9ba8e233a14297e2edbcd54eaede1b
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-10 16:54:18 +0800

    use of commit id

--- sign/sign.sh
+++ sign/sign.sh
@@ -5,11 +5,12 @@ ver=$(grep -o 'version="[0-9.]*"' config.xml | grep -o '[0-9.]*')
 verCon=$(grep -o 'android-versionCode="[^"]*"' config.xml | grep -o '[0-9]*')
 date=$(date +%m%d%Y)
 pass=${1}
-export apkName="AZ-Journey_${envApp^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
+sha=${2}
+export apkName="AZ-Journey_${envApp^^}_v${ver}_${date}_VC${verCon}_RELEASE_${sha}.apk"
 
 echo "::set-output name=apkName::$apkName"
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sign/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ionicdemo;
 mkdir release
 /usr/local/lib/android/sdk/build-tools/27.0.3/zipalign -v -p -f 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk release/${apkName}
-zip -r -j app-release.zip release
+zip -r -j app-release-${sha}.zip release
