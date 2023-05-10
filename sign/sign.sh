be87b353b9876f3a10be0e86f67c255faa53a0bd
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-10 16:49:46 +0800

    clean up

--- sign/sign.sh
+++ sign/sign.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-env="APP"
+envApp="APP"
 ver=$(grep -o 'version="[0-9.]*"' config.xml | grep -o '[0-9.]*')
 verCon=$(grep -o 'android-versionCode="[^"]*"' config.xml | grep -o '[0-9]*')
 date=$(date +%m%d%Y)
 pass=${1}
-apkName="AZ-Journey_${env^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
+export apkName="AZ-Journey_${envApp^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
+
+echo "::set-output name=apkName::$apkName"
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sign/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ionicdemo;
 mkdir release
