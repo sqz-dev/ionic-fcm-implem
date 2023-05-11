10eba7815bf3f9a05b9caedd908600f544cbddc8
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-11 17:27:57 +0800

    revert to working

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-where adb
-where npm
-where node
-where javac
-where jarsigner
-where zipalign
-where ionic
-
-# npm i;
-# ionic cordova platform add android@8;
-# # ionic cordova build android;
-# ionic cordova build android --release --prod;
+npm i;
+ionic cordova platform add android@8;
+# ionic cordova build android;
+ionic cordova build android --release --prod;
