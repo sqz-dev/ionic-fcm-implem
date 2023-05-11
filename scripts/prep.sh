7524e4bee30f8110094fa438fe11dde264f1346f
Sqz Dev <sqz.dev.ts@gmail.com>
2023-05-11 17:01:33 +0800

    self host test 2

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-npm i;
-ionic cordova platform add android@8;
-# ionic cordova build android;
-ionic cordova build android --release --prod;
+where adb
+where npm
+where node
+where javac
+where jarsigner
+where zipalign
+where ionic
+
+# npm i;
+# ionic cordova platform add android@8;
+# # ionic cordova build android;
+# ionic cordova build android --release --prod;
