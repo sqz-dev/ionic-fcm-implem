e373ce2677c47df3d8fcc92b99abfb72e7b73aed
Jhun Bryan Belen <jhun.belen@allianzpnblife.ph>
2023-05-16 13:33:37 +0800

    Update sign.ps1

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -3,13 +3,14 @@ $ver = [regex]::Match((Get-Content -Raw config.xml), 'version="([0-9.]*)"').Grou
 $verCon = [regex]::Match((Get-Content -Raw config.xml), 'android-versionCode="([^"]*)"').Groups[1].Value
 $date = Get-Date -Format "MMddyyyy"
 $pass = $args[0]
-$apkName = "AZ-Journey_${envApp.ToUpper()}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
+$apkName = "AZ-Journey_${envApp}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
 
 echo $envApp
 echo $ver
 echo $verCon
 echo $date
 echo $apkName
+echo $PWD
 
 echo "apk_name=${apkName}" >> "$GITHUB_ENV"
 
