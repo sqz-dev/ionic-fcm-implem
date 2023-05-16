151c6eec5f0c83147166d5ba019459b599e9fd44
Jhun Bryan Belen <jhun.belen@allianzpnblife.ph>
2023-05-16 16:51:16 +0800

    fixed wrong directory

--- .github/workflows/sign.sh
+++ .github/workflows/sign.sh
@@ -9,7 +9,7 @@ apkName="AZ-Journey_${envApp^^}_v${ver}_${date}_VC${verCon}_RELEASE.apk"
 
 echo "apk_name=${apkName}" >> "$GITHUB_ENV"
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .github/workflow/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ionicdemo;
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .github/workflows/ionicdemo.jks -storepass "$pass" -keypass "$pass" platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ionicdemo;
 mkdir release
 /usr/local/lib/android/sdk/build-tools/27.0.3/zipalign -v -p -f 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk release/${apkName}
 zip -r -j app-release.zip release
